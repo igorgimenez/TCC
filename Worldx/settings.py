cd16b0af0595f85f9031ad9de548ab1ad8caad42
unknown <igorgimenez1997@gmail.com>
2022-11-09 11:49:00 -0300

    Envio

--- Worldx/settings.py
+++ Worldx/settings.py
@@ -35,7 +35,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
 CSRF_TRUSTED_ORIGINS =['https://web-production-3a34.up.railway.app','https://*.127.0.0.1','https://railway.app'] 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 
 # Application definition
 
